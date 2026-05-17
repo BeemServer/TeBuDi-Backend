dd6d0db39ad041062fb01d9c920542711b8d394e
Bimo Abdul Aziz <bimoabdulaziz24@gmail.com>
2026-05-17 20:10:13 +0700

    Remove @CrossOrigin from AuthController
    Delete the CrossOrigin import and the @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true") annotation from AuthController. This removes the per-controller hardcoded CORS setting (likely to be handled via a centralized/global CORS configuration instead).

--- src/main/java/com/tebudi/TeBuDi/controller/AuthController.java
+++ src/main/java/com/tebudi/TeBuDi/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.tebudi.TeBuDi.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +23,6 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RequiredArgsConstructor
 public class AuthController {
 
